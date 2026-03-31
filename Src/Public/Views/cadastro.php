72fecb487f0db3655e3de85de294d6917e19757a
Felipe Gomes <felipeferreiraag0@gmail.com>
2026-03-31 20:56:00 -0300

    Iniciando o cadastro

--- Src/Public/Views/cadastro.php
+++ Src/Public/Views/cadastro.php
@@ -4,8 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Serviço Facil</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body>
+
+        <div class="container">
+            <div class="row">
+                <h4 class="text-center">Preencha seus dados pessoais e endereço</h4>
+                <h5 class="text-center mt-5">Foto de perfil</h5>
+                <form action="criarConta.php" method="post">
+                    <div class="images text-center">
+                        <img src="" alt="Cadastrar foto">
+                        <div class="inserir text-center">
+
+                            <input type="file" name="usuarios_imagem" id="imagem">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+
+
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
     
 </body>
 </html>
