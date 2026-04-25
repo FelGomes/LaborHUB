a2d44ba464e8420946329af632f19fb00833116b
Felipe Gomes <felipeferreiraag0@gmail.com>
2026-04-25 17:00:34 -0300

    Fazendo a telaHome de profissional

---
+++ Src/Public/Views/homeProfissional.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serviço Facil</title>
+    <link rel="stylesheet" href="../../Assets/Css/home.css">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+
+    <style>
+        
+        .lista-servico{
+            display: flex;
+            gap: 40px;
+            padding-left: 40px;
+            
+        }
+
+        .servicos-groups {
+            padding: 10px;
+            border-radius: 15px;
+            line-height: 1.2;
+            width: 300px;
+            box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.175);
+        }
+
+        .servicos-groups > h6{
+            font-size: 14pt;
+            font-weight: bold;
+            margin-bottom: 20px;
+
+            
+        }
+
+        .servicos-groups > a {
+            text-decoration: none;
+        }
+
+        .comentService {
+            color: #898585;
+        }
+
+
+    </style>
+
+
+
+
+</head>
+
+<body>
+    <header>
+
+        <div class="topo mt-3">
+            <div class="imagem">
+                <img src="../../Assets/Images/FOTOPERFIL.png" alt="Foto Escolhida">
+            </div>
+
+            <div class="sair">
+                <p class="mt-2">Deseja sair da conta? </p>
+                <button onclick="window.location.href='login.php'" class="btn-sair">Sair</button>
+            </div>
+
+        </div>
+
+        <nav>
+            <ul class="mt-1 mb-5">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Histórico</a></li>
+                <li><a href="#">Minhas avaliações</a></li>
+
+            </ul>
+
+        </nav>
+    </header>
+
+    <main>
+
+        <div class="lista-servico">
+
+            <div class="servicos-groups border">
+                <h6>Serviços Pendentes</h6>
+                <p>2 serviços</p>
+                <p class="comentService">Veja quem solicitou seu serviço</p>
+                <a href="">Acessar <i class="bi bi-arrow-right-short"></i></a>
+
+            </div>
+
+             <div class="servicos-groups border">
+                <h6>Serviços ativos</h6>
+                <p> 5 serviços ativos</p>
+                <p class="comentService">Veja detalhado seus serviços</p>
+                <a href="">Acessar <i class="bi bi-arrow-right-short"></i></a>
+
+            </div>
+
+            
+
+
+
+
+        </div>
+
+
+    </main>
+
+
+
+
+
+    <footer class="bg-dark text-center text-white py-4">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-6 col-md-12 mb-4">
+                    <h5 class="text-uppercase">Serviço Fácil</h5>
+                    <p>
+                        O sistema tem como finalidade de conectar cliente à prestadores de serviço a qualquer hora do dia
+                    </p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mb-4">
+                    <h5 class="text-uppercase">Contato</h5>
+                    <p>Email: contato@email.com</p>
+                    <p>Telefone: (00) 00000-0000</p>
+                </div>
+
+                <!-- Ícones de Redes Sociais -->
+                <section class="mb-4">
+
+                    <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/FelGomes" target="_blank" role="button">
+                        <i class="bi bi-github"></i>
+                    </a>
+
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" target="_blank" role="button">
+                        <i class="bi bi-github"></i>
+                    </a>
+
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" target="_blank" role="button">
+                        <i class="bi bi-github"></i>
+                    </a>
+
+
+                </section>
+
+                <!-- Texto de Copyright -->
+                <div class="text-center p-3">
+                    © 2026 Copyright: <a class="text-white" href="#">Equipe geral de desenvolvimento do serviço fácil</a>
+                </div>
+            </div>
+    </footer>
+
+
+
+
+
+
+    <!-- Boostrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
